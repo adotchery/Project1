c6b9a993c83bceae93c045eda30e871ee373fce4
Alan Chery <alanchery@Alans-MacBook-Pro.local>
2020-05-07 00:53:23 -0400

    added DELETE, PATCH

--- app/todo.py
+++ app/todo.py
@@ -5,10 +5,21 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 notes = []
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST','PUT','PATCH','DELETE'])
 def event():
     if request.method == 'POST':
         note = request.form.get('note')
         notes.append(note)
+def delete():
+    if request.method == 'DELETE':
+        note = request.form.get('note')
+        note.remove(note)
+# i think im going to use this method, or i was thinking combining delete and post
+def modify():
+    if request.method == 'PATCH':
+        note = request.form.get(note)
+            for note in notes
+            note.patch(notes)
+# need to include a class system or mocking?
 
     return render_template("home.html", notes=notes)
