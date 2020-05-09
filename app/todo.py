8d5d9b880ca43d550facbf8528bf15d60fb06366
Alan Chery <alanchery@Alans-MacBook-Pro.local>
2020-05-09 16:41:33 -0400

    updated delete

--- app/todo.py
+++ app/todo.py
@@ -1,25 +1,36 @@
 from flask import Flask,request,render_template,session
 from flask_session import Session
+
 app=Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
 
 notes = []
-@app.route('/', methods=['GET', 'POST','PUT','PATCH','DELETE'])
+# using counter to add ID's
+counter = 0
+@app.route('/', methods=['POST', 'GET'])
 def event():
     if request.method == 'POST':
         note = request.form.get('note')
-        notes.append(note)
-def delete():
-    if request.method == 'DELETE':
-        note = request.form.get('note')
-        note.remove(note)
-# i think im going to use this method, or i was thinking combining delete and post
-def modify():
-    if request.method == 'PATCH':
-        note = request.form.get(note)
-            for note in notes
-            note.patch(notes)
-# need to include a class system or mocking?
+        new_note = {
+            'id':counter,
+            'todo':note
+        }
+        up()
+        notes.append(new_note)
+    return render_template("home.html", notes=notes)
+
+@app.route('/delete', methods=['DELETE', 'GET'])
+def remove():
+    if  request.method == 'delete':
+        id = request.form.get('id')
+
 
     return render_template("home.html", notes=notes)
+
+def up():
+    global counter
+    counter += 1
+
+if __name__ == "__main__":
+    app.run()
